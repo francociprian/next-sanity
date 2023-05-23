e83acef61b40223c5fe8608c099ace98c49b1e6d
Franco Ciprian <franco_ciprian@hotmail.com>
2023-05-23 19:09:15 -0300

    Updating the Loading and content for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,65 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
-import { getGallery } from './api/gallery';
-import { Gallery } from '../../types';
-import { useQuery } from '@tanstack/react-query';
-import NewSlider from '../../components/NewSlider'
+import NewSlider from '../components/NewSlider'
+import { motion, useIsPresent } from 'framer-motion';
 
-type Props = {
-  gallery: Gallery[];
-};
 
 const Home: NextPage = () => {
-
-  const { data, error, isLoading, status } = useQuery<Props>({
-    queryKey: ['Gallery'],
-    queryFn: getGallery,
-    refetchInterval: 60000,
-  })
-
-  console.log(data)
-
-
-  if (error) return <main className='flex min-h-screen flex-col items-center gap-24'><div>App Failed!</div></main>
+  const isPresent = useIsPresent();
+  const [isVisible, setVisible] = useState(false);
 
   return (
-    <main className='flex justify-center items-center overflow-hidden max-sm:p-8'>
-        <NewSlider />
-      {/* <div className='flex gap-4 flex-col justify-center items-center'>
-        {data?.gallery.map((item) => (
-          <div className='' key={item._id}>
-            <h2 className='text-red-400'>{item.title}</h2>
-            <p>{item.description}</p>
-            <p>{item.publishedAt}</p>
-            <Image
-              src={item.image.url}
-              alt={item.title}
-              width={720}
-              height={580}
-              priority
-              sizes="(max-width: 640px) 100vw,
-              (max-width: 1280px) 50vw,
-              (max-width: 1536px) 33vw,
-              25vw"
-            />
-          </div>
-        ))}
-      </div> */}
+    <main className='flex justify-center items-center overflow-hidden min-h-screen max-sm:p-8 relative max-500:mx-6'>
+      <NewSlider />
+      <motion.div
+        initial={{ scaleY: 1 }}
+        animate={{ scaleY: 0, transition: { duration: 0.9, ease: "easeInOut", delay: 2 } }}
+        exit={{ scaleY: 1, transition: { duration: 0.9, ease: "easeInOut", delay: 2 } }}
+        style={{ originY: isPresent ? 0 : 1 }}
+        className="fixed top-0 left-0 right-0 bottom-0 z-[2] bg-gray-400 privacy-screen flex justify-center items-center"
+      >
+        <h3 className='flex gap-2'>
+          <motion.span
+            initial={{ opacity: 0, x: -300 }}
+            animate={{
+              opacity: isVisible ? 0 : 1,
+              x: 0,
+              y: isVisible ? -60 : 0,
+              transition: { duration: 0.95, delay: 0.1, ease: [0.08, 0.82, 0.17, 1] }
+            }}
+            transition={{ duration: 3 }}
+            onAnimationComplete={() => setVisible(true)}
+          >
+            Photographer
+          </motion.span>
+          <motion.span
+            initial={{ opacity: 0, x: -300 }}
+            animate={{
+              opacity: isVisible ? 0 : 1,
+              x: 0,
+              y: isVisible ? -60 : 0,
+              transition: { duration: 0.95, delay: 0.1, ease: [0.08, 0.82, 0.17, 1] }
+            }}
+            transition={{ duration: 3 }}
+            onAnimationComplete={() => setVisible(true)}
+          >
+            -
+          </motion.span>
+          <motion.span
+            initial={{ opacity: 0, x: -300 }}
+            animate={{
+              opacity: isVisible ? 0 : 1,
+              x: 0,
+              y: isVisible ? -60 : 0,
+              transition: { duration: 0.95, delay: 0.2, ease: [0.08, 0.82, 0.17, 1] }
+            }}
+            transition={{ duration: 3 }}
+            onAnimationComplete={() => setVisible(true)}
+          >
+            John Doe
+          </motion.span>
+        </h3>
+      </motion.div>
     </main>
   )
 }
