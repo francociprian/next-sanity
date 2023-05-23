138ca95e2b5d5940937ad258f604b3d593b634eb
Franco Ciprian <franco_ciprian@hotmail.com>
2023-05-23 18:33:11 -0300

    add animatePresence

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,14 +8,17 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AnimatePresence } from 'framer-motion';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <AnimatePresence mode='wait'>
+        <Component {...pageProps} />
+      </AnimatePresence>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   )
 }
